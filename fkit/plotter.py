a1b51bc9dc45cb0534fb94dfd31f7cdd82ac0415
Robert W <55780110+wcfrobert@users.noreply.github.com>
2023-08-19 16:35:22 -0700

    plotter update
    added docstring, made PM_interaction ACI 318 by default,

--- fkit/plotter.py
+++ fkit/plotter.py
@@ -5,7 +5,12 @@ import os
 
 
 def preview_fiber(fiber,x_limit=[-0.03, 0.03]):
-    """plot monotonic stress-strain curve of a defined fiber"""
+    """
+    Plot monotonic stress-strain curve of a defined fiber
+        fiber       patch or node fiber object
+        x_limit     max and min strain for x-axis limit
+                        OPTIONAL: default = [-0.03, 0.03]
+    """
     strain_x = np.linspace(x_limit[0],x_limit[1],500)
     stress_y = [fiber.stress_strain(a) for a in strain_x]
     
@@ -24,14 +29,19 @@ def preview_fiber(fiber,x_limit=[-0.03, 0.03]):
     return fig, strain_x, stress_y
 
 
-def preview_section(section, show_bartag=False):
-    """preview section geometry"""
+def preview_section(section, show_tag=False):
+    """
+    Preview section geometry
+        section         section object
+        show_tag        flag to show node fiber tags
+                            OPTIONAL: default = False
+    """
     # initialize
     fig, axs = plt.subplots(figsize=(11,8.5))
     for f in section.node_fibers:
         radius = (f.area/3.1415926)**(0.5)
         axs.add_patch(patches.Circle(f.coord,radius=radius,facecolor=f.default_color,edgecolor="black",zorder=2,lw=2))
-        if show_bartag:
+        if show_tag:
             axs.annotate("{}".format(f.tag), xy=(f.coord[0],f.coord[1]), xycoords='data', xytext=(0, 15), textcoords='offset points', fontsize=24, c="red")
     for f in section.patch_fibers:
         axs.add_patch(patches.Polygon(np.array(f.vertices),closed=True,facecolor=f.default_color,edgecolor="black",zorder=1,lw=1.0))
@@ -50,6 +60,10 @@ def preview_section(section, show_bartag=False):
 
 
 def plot_MK(section):
+    """
+    Plot moment curvature analysis
+        section     section object
+    """
     if not section.MK_solved:
         raise RuntimeError("Please run moment curvature analysis before plotting")
         
@@ -86,8 +100,14 @@ def plot_MK(section):
     return fig
 
 
+
 def animate_MK(section):
-    """generate a folder containing pngs which can be converted to gif"""
+    """
+    Generate a folder containing pngs which can be converted to gif
+        section     section object
+        
+    Run this in cmd with ImageMagick: "magick -delay 5 -loop 0 *.png demo.gif"
+    """
     plt.ioff()
     if not section.MK_solved:
         raise RuntimeError("Please run moment curvature analysis before animating")
@@ -134,19 +154,24 @@ def animate_MK(section):
         
         filename = os.path.join(save_dir,"frame{:04d}.png".format(i))
         fig.savefig(filename)
-        #magick -delay 5 -loop 0 *.png demo.gif
+        
 
 
-def plot_PM_ACI(section, P=[], M=[]):
+def plot_PM(section, P=None, M=None):
     """
     Plot section ACI 318 PM interaction surface (both nominal and factored)
-    Please Note:
+        section     section object
+        P           list of axial demands
+                        OPTIONAL: default = None
+        M           list of moment demand
+                        OPTIONAL: default = None
+    Note:
         Internally within fkit, the sign convention is +P = tension, -P = compression
-        This is opposite of what's commonly used within the concrete design industry.
-        For plotting and exporting purposes, the sign on P is flipped.
+        For plotting and exporting purposes, the sign on P is flipped such that the positive
+        y-axis means compression.
     """
-    if not section.PM_solved_ACI:
-        raise RuntimeError("Please run ACI interaction analysis before plotting")
+    if not section.PM_solved:
+        raise RuntimeError("Please run interaction analysis before plotting")
         
     fig, axs = plt.subplots(1,2,figsize=(16,9),gridspec_kw={'width_ratios':[1,1]})
     
@@ -167,25 +192,45 @@ def plot_PM_ACI(section, P=[], M=[]):
     axs[0].set_axisbelow(True)
     axs[0].set_aspect('equal', 'box')
 
-    # plot interaction surface
+    # plot nominal interaction surface
     # flipping P sign convention to match concrete design industry standard
     # where +P is compression, -P is tension
-    original_Mf = [m for m in section.PM_surface_ACI_factored[0][1]]
-    original_Pf = [-m for m in section.PM_surface_ACI_factored[0][0]]
-    flipped_Mf = [m for m in section.PM_surface_ACI_factored[180][1]]
-    flipped_Pf = [-m for m in section.PM_surface_ACI_factored[180][0]]
-
-    original_M = [m for m in section.PM_surface_ACI[0][1]]
-    original_P = [-m for m in section.PM_surface_ACI[0][0]]
-    flipped_M = [m for m in section.PM_surface_ACI[180][1]]
-    flipped_P = [-m for m in section.PM_surface_ACI[180][0]]
-
-    axs[1].plot(original_M, original_P, label="nominal",linestyle="--",c="red")
-    axs[1].plot(flipped_M, flipped_P, label="nominal",linestyle="--",c="red")
+    # [P,Mx,NA_depth,My,resistance_factor,phi_P,phi_Mx,phi_My]
+    M0 = [x for x in section.PM_surface[0][1]]
+    P0 = [-x for x in section.PM_surface[0][0]]
+    M180 = [-x for x in section.PM_surface[180][1]]
+    P180 = [-x for x in section.PM_surface[180][0]]
+    axs[1].plot(M0, P0, label="nominal",linestyle="-",c="black", marker=".", markersize=8)
+    axs[1].plot(M180, P180, label="nominal",linestyle="-",c="black", marker=".", markersize=8)
     
-    axs[1].plot(original_Mf, original_Pf, label="factored",c="blue")
-    axs[1].plot(flipped_Mf, flipped_Pf, label="factored",c="blue")
+    # factored interaction surface
+    # split factored curve at 0.8Po
+    Po = min(section.PM_surface[0][5])
+    for i in range(len(section.PM_surface[0][5])):
+        if section.PM_surface[0][5][i] < 0.8*Po:
+            split_index = i
+            break
     
+    M0_factored = [x for x in section.PM_surface[0][6][:split_index]]
+    P0_factored = [-x for x in section.PM_surface[0][5][:split_index]]
+    M180_factored = [-x for x in section.PM_surface[180][6][:split_index]]
+    P180_factored = [-x for x in section.PM_surface[180][5][:split_index]]
+    M0_factored.append(M0_factored[-1])
+    P0_factored.append(P0_factored[-1])
+    M180_factored.append(M0_factored[-1])
+    P180_factored.append(P180_factored[-1])
+    axs[1].plot(M0_factored, P0_factored, label="factored",linestyle="-",c="red")
+    axs[1].plot(M180_factored, P180_factored, label="factored",linestyle="-",c="red")
+    
+    # plot peak above 0.8Po with dotted line
+    M0_factored_top = [x for x in section.PM_surface[0][6][split_index:]]
+    P0_factored_top = [-x for x in section.PM_surface[0][5][split_index:]]
+    M180_factored_top = [-x for x in section.PM_surface[180][6][split_index:]]
+    P180_factored_top = [-x for x in section.PM_surface[180][5][split_index:]]
+    axs[1].plot(M0_factored_top, P0_factored_top, label="factored",linestyle="--",c="red")
+    axs[1].plot(M180_factored_top, P180_factored_top, label="factored",linestyle="--",c="red")
+    
+    # styling
     axs[1].xaxis.grid()
     axs[1].yaxis.grid()
     axs[1].axhline(0, color='black')
@@ -195,68 +240,11 @@ def plot_PM_ACI(section, P=[], M=[]):
     plt.tight_layout()
     
     # plot demands
+    P = [] if P==None else P
+    M = [] if M==None else M
     axs[1].scatter(M, P, c="red", marker="x",linewidth=2,s=100)
     
     return fig
 
 
-
-
-
-
-
-
-def plot_PM(section, P=[], M=[]):
-    """
-    Plot section PM interaction surface based on user defined fibers
-    Please Note:
-        Internally within fkit, the sign convention is +P = tension, -P = compression
-        This is opposite of what's commonly used within the concrete design industry.
-        For plotting and exporting purposes, the sign on P is flipped.
-    """
-    if not section.PM_solved:
-        raise RuntimeError("Please run interaction analysis before plotting")
-        
-    fig, axs = plt.subplots(1,2,figsize=(16,9),gridspec_kw={'width_ratios':[1,1]})
-    
-    # plot meshes
-    for f in section.node_fibers:
-        radius = (f.area/3.1415926)**(0.5)
-        axs[0].add_patch(patches.Circle(f.coord,radius=radius,facecolor=f.default_color,edgecolor="black",zorder=2))
-    for f in section.patch_fibers:
-        axs[0].add_patch(patches.Polygon(np.array(f.vertices),closed=True,facecolor=f.default_color,edgecolor="black",zorder=1,lw=1.0))
-    
-    # plot centroid
-    axs[0].scatter(section.centroid[0], section.centroid[1], c="red", marker="x",linewidth=3,s=300, zorder=3)
-    
-    # formatting
-    fig.suptitle("Section Interaction Surface")
-    axs[0].xaxis.grid()
-    axs[0].yaxis.grid()
-    axs[0].set_axisbelow(True)
-    axs[0].set_aspect('equal', 'box')
-
-    # plot interaction surface
-    # flipping P sign convention to match concrete design industry standard
-    # where +P is compression, -P is tension
-    original_M = [m for m in section.PM_surface[0][1]]
-    original_P = [-m for m in section.PM_surface[0][0]]
-    flipped_M = [m for m in section.PM_surface[180][1]]
-    flipped_P = [-m for m in section.PM_surface[180][0]]
-
-    axs[1].plot(original_M, original_P, label="nominal",linestyle="--",c="red")
-    axs[1].plot(flipped_M, flipped_P, label="nominal",linestyle="--",c="red")
-    
-    axs[1].xaxis.grid()
-    axs[1].yaxis.grid()
-    axs[1].axhline(0, color='black')
-    axs[1].axvline(0, color='black')
-    axs[1].set_xlabel("Moment")
-    axs[1].set_ylabel("Axial Force")
-    plt.tight_layout()
-    
-    # plot demands
-    axs[1].scatter(M, P, c="red", marker="x",linewidth=2,s=100)
-    
-    return fig
 
