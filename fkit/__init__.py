7600d5b5126495884b5d322b5388dc8e3f417906
Robert W <55780110+wcfrobert@users.noreply.github.com>
2025-04-05 09:36:35 -0700

    updated __init__ file
    Also imported Section object directly into fkit namespace

--- fkit/__init__.py
+++ fkit/__init__.py
@@ -1,5 +1,15 @@
+"""
+fkit (fiber kit) is a section analysis program implemented in python. It may be used to perform
+moment-curvature, and P+M interaction curve analysis of any cross section of any material type.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Robert Wang"
+__license__ = "MIT"
+
+
 import fkit.nodefiber
 import fkit.patchfiber
-import fkit.section
+from fkit.section import Section
 import fkit.sectionbuilder
 import fkit.plotter
