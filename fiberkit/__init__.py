effff2c820e840d6bc3fd7ea9290e61f4c2cb4e7
Robert W <55780110+wcfrobert@users.noreply.github.com>
2025-05-05 23:01:57 -0700

    Update __init__.py
    v2.0.0

--- fiberkit/__init__.py
+++ fiberkit/__init__.py
@@ -3,7 +3,7 @@ fkit (fiber kit) is a section analysis program implemented in python. It may be 
 moment-curvature, and P+M interaction curve analysis of any cross section of any material type.
 """
 
-__version__ = "1.1.0"
+__version__ = "2.0.0"
 __author__ = "Robert Wang"
 __license__ = "MIT"
 
